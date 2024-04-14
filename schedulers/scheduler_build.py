907cd6f2ed907b8fe5af6ca63c722cd8d9f3eafa
Anon <ym-li@outlook.com>
2024-04-14 18:12:47 -0400

    Completed DPM-Solver scheduler_build

--- schedulers/scheduler_build.py
+++ schedulers/scheduler_build.py
@@ -21,9 +21,6 @@ class BaseScheduler:
     @staticmethod
     def calculate_constants() -> Dict[str, List[tvm.nd.NDArray]]:
         raise NotImplementedError()
-    
-
-schedulers_build: List[Type[BaseScheduler]]
 
 
 def calculate_pndm_constants(num_train_timesteps: int, num_inference_steps: int, steps_offset: int, beta_start: float, beta_end: float) -> Dict[str, List[tvm.nd.NDArray]]:
@@ -137,3 +134,101 @@ class PNDMScheduler(BaseScheduler):
     @staticmethod
     def calculate_constants() -> Dict[str, List[tvm.nd.NDArray]]:
         return calculate_pndm_constants(1000, 50, 1, 0.00085, 0.012)
+        
+
+def compute_dpm_solver_multistep_scheduler_consts(num_train_timesteps: int, num_inference_steps: int, beta_start: float, beta_end: float) -> Dict[str, List[tvm.nd.NDArray]]:
+    betas = np.linspace(beta_start**0.5, beta_end**0.5, num_train_timesteps, dtype="float32") ** 2
+    alphas = 1 - betas
+    alphas_cumprod = np.cumprod(alphas, axis=0)
+    alpha_t = np.sqrt(alphas_cumprod)
+    sigma_t = np.sqrt(1 - alphas_cumprod)
+    lambda_t = np.log(alpha_t) - np.log(sigma_t)
+    
+    timesteps = np.linspace(0, num_train_timesteps - 1, num_inference_steps + 1).round().astype(int)[::-1][:-1]
+    
+    alphas, sigmas, c0s, c1s, c2s = [], [], [], [], []
+    
+    for idx, timestep in enumerate(timesteps):
+        t = timesteps[idx + 1] if idx < len(timesteps) - 1 else 0
+        s0, s1 = timesteps[idx], timesteps[idx - 1] if idx > 0 else None
+        
+        c0 = sigma_t[t] / sigma_t[s0]
+        c1 = alpha_t[t] * (np.exp(-(lambda_t[t] - lambda_t[s0])) - 1)
+        c2 = 0.5 * c1 * ((lambda_t[t] - lambda_t[s0]) / (lambda_t[s0] - lambda_t[s1])) if idx > 0 else np.array(0.0, dtype="float32")
+        
+        alphas.append(alpha_t[timestep].item())
+        sigmas.append(sigma_t[timestep].item())
+        c0s.append(c0.item())
+        c1s.append(c1.item())
+        c2s.append(c2.item())
+        
+    const_dict = {}
+    const_dict["num_steps"] = len(timesteps)
+    const_dict["timesteps"] = timesteps.tolist()
+    const_dict["alpha"] = alphas
+    const_dict["sigma"] = sigmas
+    const_dict["c0"] = c0s
+    const_dict["c1"] = c1s
+    const_dict["c2"] = c2s
+        
+    return const_dict
+        
+
+def dpm_solver_multistep_convert_model_output(sample, model_output, alpha, sigma):
+    return (sample - sigma * model_output) / alpha
+
+
+def dpm_solver_multistep_scheduler_step(sample, model_output, last_model_output, c0, c1, c2):
+    return c0 * sample - c1 * model_output - c2 * (model_output - last_model_output)
+
+
+def generate_dpm_solver_multistep_scheduler_steps() -> tvm.IRModule:
+    def emit_convert_model_output(bb):
+        sample = relax.Var("sample", R.Tensor((1, 4, 64, 64), "float32"))
+        model_output = relax.Var("model_output", R.Tensor((1, 4, 64, 64), "float32"))
+        alpha = relax.Var("alpha", R.Tensor((), "float32")) 
+        sigma = relax.Var("sigma", R.Tensor((), "float32"))
+        
+        with bb.function("dpm_solver_multistep_scheduler_conversion_output", [sample, model_output, alpha, sigma]):
+            converted_model_output = bb.emit(dpm_solver_multistep_convert_model_output(sample, model_output, alpha, sigma), "converted_model_output")
+            bb.emit_func_output(converted_model_output)
+            
+    def emit_scheduler_step(bb):        
+        sample = relax.Var("sample", R.Tensor((1, 4, 64, 64), "float32"))
+        model_output = relax.Var("model_output", R.Tensor((1, 4, 64, 64), "float32"))
+        last_model_output = relax.Var("last_model_output", R.Tensor((1, 4, 64, 64), "float32"))
+        c0 = relax.Var("c0", R.Tensor((), "float32"))
+        c1 = relax.Var("c1", R.Tensor((), "float32")) 
+        c2 = relax.Var("c2", R.Tensor((), "float32"))
+        
+        with bb.function("dpm_solver_multistep_scheduler_step", [sample, model_output, last_model_output, c0, c1, c2]):
+            prev_sample = bb.emit(dpm_solver_multistep_scheduler_step(sample, model_output, last_model_output, c0, c1, c2), "prev_sample")
+            bb.emit_func_output(prev_sample)
+            
+    bb = relax.BlockBuilder()
+    emit_convert_model_output(bb)
+    emit_scheduler_step(bb)
+    
+    return bb.get()
+            
+
+class DPMSolverMultistepScheduler(BaseScheduler):
+    constants_file_name = "dpm_solver_multistep_scheduler_constants.json"
+
+    @staticmethod
+    def scheduler_steps() -> tvm.IRModule:
+        return generate_dpm_solver_multistep_scheduler_steps()
+    
+    @staticmethod
+    def list_step_functions() -> List[str]:
+        return [
+            "dpm_solver_multistep_scheduler_conversion_output",
+            "dpm_solver_multistep_scheduler_step",
+        ]
+        
+    @staticmethod
+    def calculate_constants() -> Dict[str, List[tvm.nd.NDArray]]:
+        return compute_dpm_solver_multistep_scheduler_consts(1000, 20, 0.00085, 0.012)
+        
+
+schedulers_build: List[Type[BaseScheduler]] = [DPMSolverMultistepScheduler, PNDMScheduler]
